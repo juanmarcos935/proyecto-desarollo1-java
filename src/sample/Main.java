320242ee79e5bbe48c198f4eb089f561b986f0df
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-02-23 11:05:38 -0500

    Cambiando dimensiones

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Sistema de Gestion de Informacion CELUSOFT");
-        primaryStage.setScene(new Scene(root, 750, 625));
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 

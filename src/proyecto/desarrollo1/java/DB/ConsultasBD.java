264faaf05d8f4846895fd6889140399c3e182a68
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-02-25 20:57:35 -0500

    Añadiendo consulta de Registro de Usuarios

--- src/proyecto/desarrollo1/java/DB/ConsultasBD.java
+++ src/proyecto/desarrollo1/java/DB/ConsultasBD.java
@@ -40,16 +40,33 @@ public class ConsultasBD {
         Statement st = connec.createStatement();
         ResultSet rs = st.executeQuery(consulta_sql_login);
         if(rs.next())
-        {
-            String consulta_sql2 = "SELECT usuario_tipo FROM usuario WHERE usuario_login='" + login + "' AND usuario_password='" + password + "';";
+        {   
+            System.out.println("Por ahora login y contraseña exitosos");
+            String consulta_sql_login2 = "SELECT usuario_tipo FROM usuario WHERE usuario_login='" + login + "' AND usuario_password='" + password + "';";
             Statement st2 = connec.createStatement();
-            ResultSet rs2 = st2.executeQuery(consulta_sql2);
+            ResultSet rs2 = st2.executeQuery(consulta_sql_login2);
             String tipo = rs2.getString("usuario_tipo");
             return tipo;
         }
         else
         {
             return "FALLIDO";
+        }
+    }
+    
+    public void registrarUsuario(Usuario user) throws SQLException
+    {
+        int operacion;
+        String consulta_sql_registrar_usuario = "INSERT INTO usuario (id_usuario, usuario_tipo, usuario_login, usuario_password, usuario_nombre, usuario_apelldios, usuario_cc, usuario_telefono, usuario_estado) VALUES (default, '" + user.getTipo() + "', '" + user.getLogin() + "', '" + user.getPassword() + "', '" + user.getNombre() + "', '" + user.getApellido() + "', " + user.getCedula() + ", " + user.getTelefono() + ", " + user.isActivo() + ");";
+        try
+        {
+            Statement st = connec.createStatement();
+            operacion = st.executeUpdate(consulta_sql_registrar_usuario);
+            System.out.println("Registro de usuario exitoso");
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
         }
     }
     

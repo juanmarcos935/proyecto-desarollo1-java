01263f38eb0f87b33246059d3cd05c04319b5d9b
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-02-25 20:26:21 -0500

    Pensando en DAOS

--- src/proyecto/desarrollo1/java/Control/DAOUsuario.java
+++ src/proyecto/desarrollo1/java/Control/DAOUsuario.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.desarrollo1.java.Control;
 
-/**
- *
- * @author chatarrita
- */
+import proyecto.desarrollo1.java.DB.ConsultasBD;
+
+
 public class DAOUsuario {
+    
+    
     
 }

a776bc8747a37bd7a971a511eb8404dfcfeb9f34
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-02-25 21:05:25 -0500

    Comenzando DAO Usuario

--- src/proyecto/desarrollo1/java/Control/DAOUsuario.java
+++ src/proyecto/desarrollo1/java/Control/DAOUsuario.java
@@ -5,10 +5,20 @@
  */
 package proyecto.desarrollo1.java.Control;
 
-/**
- *
- * @author chatarrita
- */
+import java.sql.SQLException;
+import proyecto.desarrollo1.java.DB.ConsultasBD;
+import proyecto.desarrollo1.java.Modelo.Usuario;
+
 public class DAOUsuario {
+    
+    private ConsultasBD consultorBD = new ConsultasBD();
+    
+    public void registrarUsuario(int id, String tipo, String login, String password, String nombre, String apellido, int cedula, int telefono, boolean activo) throws SQLException
+    {
+        Usuario user = new Usuario(id, tipo, login, password, nombre, apellido, cedula, telefono, activo);
+        consultorBD.empezarConexion();
+        consultorBD.registrarUsuario(user);
+        consultorBD.cerrarConexion();
+    }
     
 }

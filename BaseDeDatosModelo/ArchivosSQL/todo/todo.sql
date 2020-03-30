4e7d0b7f46b2395817897c39643785a363c890e7
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-03-29 23:23:13 -0500

    Mejorando .SQL

--- BaseDeDatosModelo/ArchivosSQL/todo/todo.sql
+++ BaseDeDatosModelo/ArchivosSQL/todo/todo.sql
@@ -26,6 +26,57 @@ ALTER TABLE usuario ADD CONSTRAINT nueva_restricción_única_usuarioCC UNIQUE (u
 
 
 --
+-- TABLE: usuario_operador
+-- 
+--  
+
+CREATE TABLE usuario_operador (
+  id_usuario int NOT NULL 
+);
+
+-- 
+
+
+-- 
+ALTER TABLE usuario_operador ADD CONSTRAINT nueva_restricción_única_usu_ope PRIMARY KEY (id_usuario);
+
+-- 
+ALTER TABLE usuario_operador ADD CONSTRAINT nueva_restricción_fclave_opera_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+--
+-- TABLE: usuario_administrador
+-- 
+--  
+
+CREATE TABLE usuario_administrador (
+  id_usuario int NOT NULL 
+);
+
+-- 
+ALTER TABLE usuario_administrador ADD CONSTRAINT nueva_restricción_fclave_admin_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+--
+-- TABLE: usuario_gerente
+-- 
+--  
+
+CREATE TABLE usuario_gerente (
+  id_usuario int NOT NULL 
+);
+
+-- 
+
+
+-- 
+ALTER TABLE usuario_gerente ADD CONSTRAINT nueva_restricción_usuario_gerente PRIMARY KEY (id_usuario);
+
+-- 
+ALTER TABLE usuario_gerente ADD CONSTRAINT nueva_restricción_fclave_usuario_gerente_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+--
 -- TABLE: banco
 -- 
 --  
